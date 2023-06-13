9e3c8622b190aea02f9356617e9617ed964a6dcd
gusjd109 <hyunsung109@gmail.com>
2023-06-13 13:31:45 +0000

    PROJECT2 : ALL PASS

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -131,7 +131,7 @@ void
 check_address (void *addr) {
 	// 현재 접근하는 메모리 주소가 NULL이거나, 커널 영역에서 사용하는 주소이거나, 유저 영역에서 사용하는 주소이지만 페이지로 할당되지 않은 주소일 경우(=잘못된 접근)
 	if (addr == NULL || is_kernel_vaddr(addr) || pml4_get_page (thread_current ()->pml4, addr) == NULL) {
-		exit(-1); // 프로세스 종료
+		exit (-1); // 프로세스 종료
 	}
 }
 
